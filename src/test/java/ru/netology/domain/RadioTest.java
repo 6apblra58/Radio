7becabe2df2a3d9ec318a2d04cc5ec8075a4ed12
Baryshnikov Dmitry <6apblra58@gmail.com>
2021-02-17 12:04:24 +0300

    added tests

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,4 +1,104 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RadioTest {
+    @Test
+    public void createRadio() {
+        Radio radio = new Radio();
+        String expected = "RadioPhilips";
+        assertNull(radio.getName());
+        radio.setName(expected);
+        assertEquals(expected, radio.getName());
+    }
+    @Test
+    public void changeCertainRadioStation() {
+        Radio station = new Radio();
+        station.setCurrentRadioStation(7);
+        assertEquals(7, station.getCurrentRadioStation());
+
+    }
+
+    @Test
+    public void changeOverLastRadioStation() {
+        Radio station = new Radio();
+        station.setCurrentRadioStation(9);
+        station.nextRadioStation();
+        assertEquals(0, station.getCurrentRadioStation());
+
+    }
+
+    @Test
+    public void changeUnderInitialRadioStation() {
+        Radio station = new Radio();
+        station.setCurrentRadioStation(0);
+        station.prevRadioStation();
+        assertEquals(9, station.getCurrentRadioStation());
+
+    }
+
+    @Test
+    public void nextRadioStation() {
+        Radio station = new Radio();
+        station.setCurrentRadioStation(6);
+        station.nextRadioStation();
+        assertEquals(7, station.getCurrentRadioStation());
+    }
+
+    @Test
+    public void prevRadioStation() {
+        Radio station = new Radio();
+        station.setCurrentRadioStation(6);
+        station.prevRadioStation();
+        assertEquals(5, station.getCurrentRadioStation());
+    }
+
+    @Test
+    public void volumeUpOverMax() {
+        Radio volume = new Radio();
+        volume.setVolume(10);
+        volume.volumeUp();
+        assertEquals(10, volume.getVolume());
+    }
+    @Test
+    public void volumeOverMax() {
+        Radio volume = new Radio();
+        volume.setVolume(11);
+        volume.volumeUp();
+        assertEquals(10, volume.getVolume());
+        System.out.println(1);
+    }
+
+
+    @Test
+    public void volumeDownUnderMin() {
+        Radio volume = new Radio();
+        volume.setVolume(-1);
+        volume.volumeDown();
+        assertEquals(0, volume.getVolume());
+    }
+    @Test
+    public void volumeZero() {
+        Radio volume = new Radio();
+        volume.setVolume(0);
+        volume.volumeDown();
+        assertEquals(0, volume.getVolume());
+    }
+
+    @Test
+    public void volumeDown() {
+        Radio volume = new Radio();
+        volume.setVolume(7);
+        volume.volumeDown();
+        assertEquals(6, volume.getVolume());
+    }
+
+    @Test
+    public void volumeUp() {
+        Radio volume = new Radio();
+        volume.setVolume(7);
+        volume.volumeUp();
+        assertEquals(8, volume.getVolume());
+    }
 }
